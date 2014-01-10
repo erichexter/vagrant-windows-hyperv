0cf06b0b11441fb9fbeb1c6bbaef072fd19b11e3
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-01-10 16:27:25 +0530

    PowerShell script to sync files

---
+++ lib/hyper-v/scripts/file_sync.ps1
@@ -0,0 +1,77 @@
+function Get-file-hash($source_path, $delimiter) {
+    $source_files = @()
+    (Get-ChildItem $source_path -rec | ForEach-Object -Process {
+      Get-FileHash -Path $_.FullName -Algorithm MD5 } ) |
+        ForEach-Object -Process {
+          $source_files += ($_.Path -replace $source_path, "") + $delimiter + $_.Hash
+        }
+    $source_files
+}
+
+function Get-Remote-Session() {
+    $username = "vagrant"
+    $password = "happy"
+    $secstr = convertto-securestring -AsPlainText -Force -String $password
+    $cred = new-object -typename System.Management.Automation.PSCredential -argumentlist $username, $secstr
+    New-PSSession -ComputerName 10.18.20.62 -Credential $cred
+}
+
+function Get-remote-file-hash($source_path, $delimiter) {
+    $session = Get-Remote-Session
+    Write-Host "Call remote command"
+    Invoke-Command -Session $session -ScriptBlock ${function:Get-file-hash} -ArgumentList $source_path, $delimiter
+    # Always remove the connection after Use
+    Remove-PSSession -ComputerName 10.18.20.62 -Id $session.Id
+}
+
+function Sync-Remote-Machine($remove_files, $copy_files, $source_root_path, $destination_root_path) {
+    ForEach ($item in $copy_files) {
+      $from = $source_root_path + $item
+      $to = $destination_root_path + $item
+      Copy-VMFile "Super-Win_orig" -SourcePath $from -DestinationPath $to -CreateFullPath -FileSource Host -Force
+    }
+}
+
+function Create-Remote-Folders($empty_source_folders, $destination_root_path) {
+    ForEach ($item in $empty_source_folders) {
+        $new_name =  $destination_root_path + $item
+        New-Item "$new_name" -type directory -Force
+    }
+}
+
+function Get-Empty-folders-From-Source($source_root_path) {
+  Get-ChildItem $source_root_path -recurse |
+        Where-Object {$_.PSIsContainer -eq $True} |
+            Where-Object {$_.GetFiles().Count -eq 0} |
+                Select-Object FullName | ForEach-Object -Process {
+                    $empty_source_folders += ($_.FullName -replace $source_root_path, "")
+                }
+}
+
+$delimiter = " || "
+$source_root_path = "E:\\Test_Sync"
+$destination_root_path = "C:\\Users\\Vagrant\\Desktop\\Test_Sync"
+
+$source_files = Get-file-hash $source_root_path $delimiter
+$destination_files = Get-remote-file-hash $destination_root_path $delimiter
+
+# Compare source and destination files
+$remove_files = @()
+$copy_files = @()
+Compare-Object -ReferenceObject $source_files -DifferenceObject $destination_files | ForEach-Object {
+  if ($_.SideIndicator -eq "=>") {
+      $remove_files += $_.InputObject.Split($delimiter)[0]
+  } else {
+      $copy_files += $_.InputObject.Split($delimiter)[0]
+  }
+}
+
+# Update the files to remote machine
+Sync-Remote-Machine $remove_files $copy_files $source_root_path $destination_root_path
+
+# Create any empty folders which missed to sync to remote machine
+$empty_source_folders = @()
+$directories = Get-Empty-folders-From-Source $source_root_path
+$session = Get-Remote-Session
+$result = Invoke-Command -Session $session -ScriptBlock ${function:Create-Remote-Folders} -ArgumentList $empty_source_folders, $destination_root_path
+Remove-PSSession -ComputerName 10.18.20.62 -Id $session.Id
