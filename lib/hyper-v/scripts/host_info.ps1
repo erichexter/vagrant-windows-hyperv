d219ad252078ca5812f749ad0b74f4f50218e9a2
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-01-31 15:07:40 +0530

    Return only Host IP

--- lib/hyper-v/scripts/host_info.ps1
+++ lib/hyper-v/scripts/host_info.ps1
@@ -1,5 +1,4 @@
 $hostname =  $(whoami)
-$username = $hostname.split('\')[1] + "@" + $hostname.split('\')[0]
 $ip = (Get-WmiObject -class win32_NetworkAdapterConfiguration -Filter 'ipenabled = "true"').ipaddress[0]
 Write-Host "===Begin-Output==="
 Write-Host "{
