3426f0b6ca6e2846ae0dbe4def50f62163f1e2da
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-02-14 12:03:21 +0530

    Cleanup for linux SMB share

--- lib/hyper-v/scripts/set_smb_share.ps1
+++ lib/hyper-v/scripts/set_smb_share.ps1
@@ -15,26 +15,7 @@ $modules = @()
 $modules += $presentDir + "\utils\write_messages.ps1"
 forEach ($module in $modules) { . $module }
 
-# Get a sample folder which has the required ACL (Access control)
-# Use this as a template and assign the same access to the folders which are
-# to be shared from Host to the Guest VM
-
 try {
-  # http://technet.microsoft.com/en-us/library/ff730951.aspx
-  function Set-Acl-Rule($host_share_username) {
-    $colRights = [System.Security.AccessControl.FileSystemRights]"Read, Modify, FullControl"
-
-    $InheritanceFlag = [System.Security.AccessControl.InheritanceFlags]::None
-    $PropagationFlag = [System.Security.AccessControl.PropagationFlags]::None
-
-    $objType =[System.Security.AccessControl.AccessControlType]::Allow
-    $computer_name = $(Get-WmiObject Win32_Computersystem).name
-    $objUser = New-Object System.Security.Principal.NTAccount("$computer_name\$host_share_username")
-
-    return New-Object System.Security.AccessControl.FileSystemAccessRule `
-        ($objUser, $colRights, $InheritanceFlag, $PropagationFlag, $objType)
-  }
-
   # See all available shares and check alert user for existing / conflicting share name
   $shared_folders = net share
   $reg = "$share_name(\s+)$path(\s)"
@@ -43,21 +24,6 @@ try {
     # Always clear the existing share name and create a new one
     net share $share_name /delete /y
   }
-
-  # Set ACL for all files in the folder
-  foreach ($file in $(Get-ChildItem $path -recurse)) {
-    $acl = get-acl $file.FullName
-
-    $permissions = Set-Acl-Rule $host_share_username
-    $acl.SetAccessRule($permissions)
-
-    # Write the changes to the object
-    try {
-      set-acl $File.Fullname $acl -ErrorAction stop
-      } catch {
-
-      }
-    }
 
   $computer_name = $(Get-WmiObject Win32_Computersystem).name
   $grant_permission = "$computer_name\$host_share_username,Full"
@@ -70,7 +36,7 @@ try {
   } else {
     $reg = "^$share_name(\s+)"
     $existing_share = $shared_folders -Match $reg
-    Write-Error-Message "Conflicting share name, A share name already exist $existing_share"
+    Write-Error-Message "IGNORING Conflicting share name, A share name already exist $existing_share"
   }
 } catch {
   Write-Error-Message $_
