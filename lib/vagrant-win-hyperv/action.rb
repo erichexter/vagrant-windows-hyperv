fd293e61cf54646ecf7a2e3cd0d7d7bc8ba163c8
Ramakrishnan <ramakrishnan.v@happiestminds.com>
2014-03-26 11:11:58 +0530

    Use graceful halt before package.

--- lib/vagrant-win-hyperv/action.rb
+++ lib/vagrant-win-hyperv/action.rb
@@ -20,15 +20,13 @@ module VagrantPlugins
               next
             end
             b2.use SetupPackageFiles
-            b2.use action_halt
-            b2.use Call, WaitForState, :off, 120 do |env2, b3|
-              if env2[:result]
-                b3.use Export
-                b3.use Package
-              else
-                env2[:ui].info("Machine did not reload, Check machine's status")
+            b2.use Call, GracefulHalt, :off, :running do |env2, b3|
+              if !env2[:result]
+                b3.use StopInstance
               end
             end
+            b2.use Export
+            b2.use Package
           end
         end
       end
@@ -56,8 +54,6 @@ module VagrantPlugins
 
       # The autoload farm
       action_root = Pathname.new(File.expand_path("../action", __FILE__))
-      autoload :WaitForState, action_root.join('wait_for_state')
-      autoload :SSHExec, action_root.join('ssh_exec')
       autoload :Export, action_root.join("export")
       autoload :Package, action_root.join("package")
       autoload :Provision, action_root.join('provision')
